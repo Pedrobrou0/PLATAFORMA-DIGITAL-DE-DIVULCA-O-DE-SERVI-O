c34db515cb7854ef0332d44651b815583199d410
Davy Henguel <davyalmeida2008@gmail.com>
2026-09-21 22:00:46 -0300

    Implementa views de listar e detalhar feedback

--- suporte/views.py
+++ suporte/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Feedback, Avaliacao
+from .forms import FeedbackForm, AvaliacaoForm
 
-# Create your views here.
+
+def lista_feedbacks(request):
+    feedbacks = Feedback.objects.all()
+
+    return render(request, 'suporte/feedback_lista.html', {
+        'feedbacks': feedbacks
+    })
+
+
+def detalhe_feedback(request, id):
+    feedback = Feedback.objects.get(id=id)
+
+    return render(request, 'suporte/feedback_detalhe.html', {
+        'feedback': feedback
+    })
